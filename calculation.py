1f63fcf602dac17a388d7ac0d08465995903c090
tung.nguyendinh <tung.nguyendinh@pixta.co.jp>
2021-03-05 16:05:49 +0700

    feat(calculation.py) add rectangle perimeter

--- calculation.py
+++ calculation.py
@@ -1,5 +1,8 @@
 import math
 
+def cal_rectangle_perimeter(a, b):
+    return 2*(a + b)
+
 def cal_circle_area(r):
     return math.pi * pow(r, 2)
 
@@ -8,6 +11,7 @@ def cal_rectangle_area(a, b):
 
 if __name__ == '__main__':
     f = int(input("choose function: \n\
+        0. cal_rectangle_perimeter \n\
         1. cal_circle_area \n\
         2. cal_rectangle_area \nPlease enter an integer:"))
 
@@ -15,7 +19,12 @@ if __name__ == '__main__':
     add = int(input("Add addition:"))
     sub = int(input("Add subtraction:"))
 
-    if f == 1:
+    if f == 0:
+        a = int(input("Input value a: "))
+        b = int(input("Input value b: "))
+        result = cal_rectangle_perimeter(a, b)
+
+    elif f == 1:
         r = int(input("Input value r: "))
         result = cal_circle_area(r)
     elif f == 2:
